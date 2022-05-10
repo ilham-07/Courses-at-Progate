67137b05607e6b95abe56587d7d31a2cf5219e71
Ilham <87804843+ilham-07@users.noreply.github.com>
2022-05-10 10:12:20 +0700

    Add files via upload

---
+++ Python/python_study_5/page7/script.py
@@ -0,0 +1,11 @@
+from food import Food
+from drink import Drink
+
+# Tambahkan argument ke Food()
+food1 = Food('Roti Lapis', 5,330)
+
+print(food1.info())
+
+drink1 = Drink('Kopi', 3)
+drink1.volume = 180
+print(drink1.info())
---
+++ Python/python_study_5/page9/script.py
@@ -0,0 +1,10 @@
+from food import Food
+from drink import Drink
+
+food1 = Food('Roti Lapis', 5, 330)
+print(food1.info())
+
+# Tambahakan argument ke Drink()
+drink1 = Drink('Kopi', 3, 180)
+
+print(drink1.info())
